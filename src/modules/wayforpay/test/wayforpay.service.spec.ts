14e158f7a022569fd6b53620bebe56831c1146fa
rikibob <bobiar0804@gmail.com>
2024-10-11 00:37:32 +0300

    finished create test for wayforpay service

--- src/modules/wayforpay/test/wayforpay.service.spec.ts
+++ src/modules/wayforpay/test/wayforpay.service.spec.ts
@@ -5,7 +5,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { WayforpayEntity } from '../../../entitties/wayforpay.entity';
 import axios, { AxiosResponse } from 'axios';
 import { SaveWayForPayDto } from '../dtos/save-wayforpay.dto';
-import { ConflictException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
 
 describe('WayforpayService', () => {
   let service: WayforpayService;
@@ -85,15 +90,23 @@ describe('WayforpayService', () => {
       expect(result).toEqual(responseMock);
     });
 
-    it('should throw an error if sending payment request fails', async () => {
+    it('BadRequestException Failed to send payment request.', async () => {
       const data = { productName: 'SampleProduct', amount: 100 };
 
-      jest
-        .spyOn(axios, 'post')
-        .mockRejectedValue(new Error('Failed to send payment request.'));
+      const responseMock: AxiosResponse = {
+        data: 'sample-response-data',
+        status: 400,
+        statusText: 'Bad Request',
+        headers: {},
+        config: {
+          headers: undefined,
+        },
+      };
 
-      await expect(service.sendPaymentRequest(data)).rejects.toThrowError(
-        'Failed to send payment request.',
+      jest.spyOn(axios, 'post').mockRejectedValue(responseMock);
+
+      await expect(service.sendPaymentRequest(data)).rejects.toThrow(
+        BadRequestException,
       );
     });
   });
@@ -145,6 +158,150 @@ describe('WayforpayService', () => {
       expect(mockRepository.findOneBy).toHaveBeenCalledWith({
         user_id: req.user.uuid,
       });
+    });
+  });
+
+  describe('deleteInfoPay', () => {
+    it('should delete the information with DB', async () => {
+      const userId = 'user-uuid';
+      const currentUser = 'user-uuid';
+
+      jest.spyOn(mockRepository, 'delete').mockResolvedValue({});
+      jest
+        .spyOn(mockRepository, 'findOneBy')
+        .mockResolvedValue({ user_id: userId });
+
+      await service.deleteInfoPay(userId, currentUser);
+      expect(mockRepository.findOneBy).toHaveBeenCalledWith({
+        user_id: userId,
+      });
+      expect(mockRepository.delete).toHaveBeenCalledWith({ user_id: userId });
+    });
+
+    it('should throw NotFoundException User with ID ${userId} not found info pay', async () => {
+      const userId = 'user-uuid';
+      const currentUser = 'user-uuid';
+
+      jest.spyOn(mockRepository, 'findOneBy').mockResolvedValue(null);
+
+      await expect(service.deleteInfoPay(userId, currentUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw ForbiddenException You do not have permission to access info pay.', async () => {
+      const userId = 'user-uuid';
+      const currentUser = ' ';
+
+      jest.spyOn(mockRepository, 'findOneBy').mockResolvedValue({});
+
+      await expect(service.deleteInfoPay(userId, currentUser)).rejects.toThrow(
+        ForbiddenException,
+      );
+    });
+  });
+
+  describe('checkInfoPayAndCreate', () => {
+    it('should set value merchantAccount and merchantSecret', async () => {
+      const userId = 'user-uuid';
+
+      jest.spyOn(mockRepository, 'findOneBy').mockResolvedValue({
+        decrypt: (merchantSecret) => {
+          return merchantSecret;
+        },
+        merchantAccount: 'merchantAccount',
+        merchantSecret: 'merchantSecret',
+      });
+
+      await service.checkInfoPayAndCreate(userId);
+
+      expect(service.merchantAccount).toBe('merchantAccount');
+      expect(service.merchantSecret).toBe('merchantSecret');
+    });
+
+    it('should throw NotFoundException User with ID ${userId} not found info pay', async () => {
+      const userId = 'user-uuid';
+
+      jest.spyOn(mockRepository, 'findOneBy').mockResolvedValue(null);
+
+      await expect(service.checkInfoPayAndCreate(userId)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw ForbiddenException User with ID ${userId} not found info pay', async () => {
+      const userId = 'user-uuid';
+
+      jest.spyOn(mockRepository, 'findOneBy').mockResolvedValue({
+        decrypt: (merchantSecret) => {
+          return merchantSecret;
+        },
+        merchantSecret: null,
+      });
+
+      await expect(service.checkInfoPayAndCreate(userId)).rejects.toThrow(
+        ForbiddenException,
+      );
+    });
+  });
+
+  describe('generateTransactionSignature', () => {
+    it('should generate a valid order signature', () => {
+      const data = { productName: 'SampleProduct', amount: 100 };
+      const result = service.generateOrderSignature(data);
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('transactionList', () => {
+    it('should send a payment request successfully', async () => {
+      const data = { dateBegin: 1728518400000, dateEnd: 1727731200000 };
+      const responseMock: AxiosResponse = {
+        data: {
+          transactionList: [],
+        },
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config: {
+          headers: undefined,
+        },
+      };
+
+      jest.spyOn(axios, 'post').mockResolvedValue(responseMock);
+
+      const result = await service.transactionList(data);
+      expect(result).toEqual(responseMock.data.transactionList);
+    });
+
+    it('should throw BadRequestException Failed to send transaction list request.', async () => {
+      const data = {};
+
+      const responseMock: AxiosResponse = {
+        data: {
+          data: 'sample-response-data',
+        },
+        status: 400,
+        statusText: 'Bad Request',
+        headers: {},
+        config: {
+          headers: undefined,
+        },
+      };
+
+      jest.spyOn(axios, 'post').mockRejectedValue(responseMock);
+
+      await expect(service.transactionList(data)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+
+    it('should throw BadRequestException Dates exceed 31 days', async () => {
+      const data = { dateBegin: 1727731200000, dateEnd: 1730582400000 };
+
+      await expect(service.transactionList(data)).rejects.toThrow(
+        BadRequestException,
+      );
     });
   });
 });
